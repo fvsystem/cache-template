30e11db27dac0007494991e4e3224dbddb710cc2
Lucas Rego <lucas.raziel@gmail.com>
2022-05-13 01:34:17 -0300

    configure jest

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  clearMocks: true,
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  collectCoverage: true,
+  //   collectCoverageFrom: ['src/fake/**/*.{ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 80,
+      statements: 100,
+    },
+  },
+};
