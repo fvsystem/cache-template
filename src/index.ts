af713b5860c6ccac1bbb11fdd5208b138a20a4f3
Lucas Rego <lucas.raziel@gmail.com>
2022-05-13 21:15:09 -0300

    version 0.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Data from './dto/data';
+import SaveOptions from './dto/saveOptions';
 import CacheTemplate from './template/CacheTemplate';
 import FakeCacheProvider from './fake/FakeCacheProvider';
 
-export { Data, CacheTemplate, FakeCacheProvider };
+export { Data, CacheTemplate, FakeCacheProvider, SaveOptions };
 
 export default CacheTemplate;
